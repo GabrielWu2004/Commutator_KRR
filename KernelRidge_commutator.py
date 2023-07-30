a88d201a7b297c72f6d07c22f12f0ac878042ec5
Gabriel Wu <gabrielwu2004@gmail.com>
2023-07-30 17:40:36 -0400

    initial commit

---
+++ KernelRidge_commutator.py
@@ -0,0 +1,75 @@
+import numpy as np
+from scipy.linalg import cho_solve
+import numba
+
+@numba.jit(nopython=True)
+def p_distance_commutator(X,Y):
+    a, b = len(X), len(Y)
+    mat = np.zeros((a,b))
+    for i in range(a):
+        for j in range(b):
+            d = X[i]@Y[j] - Y[j]@X[i]
+            mat[i][j] = np.linalg.norm(d)
+    return mat
+
+def KRR_commutator(X_train,Y_train,X_test,best_params,kernel='rbf',dist1='na',dist2='na'):
+    lam = best_params['lambda']
+    params = best_params
+    if type(dist1)==str:
+        dist1=p_distance_commutator(X_train,X_train)
+    K=covariance(dist1,kernel,params)
+    K+=(np.eye(K.shape[0])*lam)
+    try:
+        L=np.linalg.cholesky(K)
+    except:
+        return 'Gram Matrix is not positive definite'
+    else:
+        try:
+            alpha=cho_solve((L,True),Y_train)
+        except:
+            return 'Cholesky decomposition failed, check distance matrices'
+        else:
+            if type(dist2)==str:
+                dist2=p_distance_commutator(X_train,X_test)
+            k=covariance(dist2,kernel,params)
+            return np.dot(k.T,alpha)
+
+@numba.jit(nopython=True)
+def generate_CM(cood,charges,pad):
+    size=len(charges)
+    cm=np.zeros((pad,pad))
+    for i in range(size):
+        for j in range(size):
+            if i==j:
+                cm[i,j]=0.5*(charges[i]**(2.4))
+            else:
+                dist=np.linalg.norm(cood[i,:]-cood[j,:])
+                
+                cm[i,j]=(charges[i]*charges[j])/dist
+    summation = np.array([sum(x**2) for x in cm])
+    sorted_mat = cm[np.argsort(summation)[::-1,],:]    
+    return sorted_mat
+
+def covariance(dist,kernel,params):
+    if kernel=='linear':
+        K=(params['sigma0']**2)+((params['sigma1']**2)*dist)
+        return K
+    elif kernel=='polynomial':
+        K=((params['sigma0']**2)+((params['sigma1']**2)*dist))**params['order']
+        return K
+    elif kernel in ['rbf','gaussian','Gaussian']:
+        dist=dist/(params['length'])
+        return np.exp(-(dist**2)/2)
+    elif kernel=='laplacian':
+        dist=dist/(params['length'])
+        return np.exp(-dist)
+    elif kernel=='matern1':
+        dist=(3**0.5)*dist/(params['length'])
+        return (1+dist)*np.exp(-dist)
+    elif kernel=='matern2':
+        dist1=(5**0.5)*dist/(params['length'])
+        dist2=5*(dist**2)/(3*(params['length']**2))
+        return (1+dist1+dist2)*np.exp(-dist)
+    elif kernel=='rq':
+        dist=(dist**2)/(2*params['alpha']*(params['length']**2))
+        return (1+dist)**(-params['alpha'])
