b59766724065a25a968badab09d96a743dc8cf98
Gabriel Wu <gabrielwu2004@gmail.com>
2023-07-31 15:45:30 -0400

    new learning curve and distribution

--- KernelRidge_commutator.py
+++ KernelRidge_commutator.py
@@ -12,6 +12,22 @@ def p_distance_commutator(X,Y):
             mat[i][j] = np.linalg.norm(d)
     return mat
 
+def p_distance_eigval_batch(X, Y, batch_size=100):
+    a, b = len(X), len(Y)
+    mat = np.zeros((a,b))
+    for i in range(0, a, batch_size):
+        for j in range(0, b, batch_size):
+            end_idx_i = min(i + batch_size, a)
+            end_idx_j = min(j + batch_size, b)  
+            eigvals_X = np.linalg.eigvals(X[i:end_idx_i])
+            eigvals_Y = np.linalg.eigvals(Y[j:end_idx_j])
+            eigvals_X = eigvals_X[:, np.newaxis, :]
+            eigvals_Y = eigvals_Y[np.newaxis, :, :]
+            d_eig = eigvals_X - eigvals_Y
+            mat_batch = np.linalg.norm(d_eig, axis=2)
+            mat[i:end_idx_i, j:end_idx_j] = mat_batch
+    return mat
+
 def KRR_commutator(X_train,Y_train,X_test,best_params,kernel='rbf',dist1='na',dist2='na'):
     lam = best_params['lambda']
     params = best_params
@@ -31,6 +47,28 @@ def KRR_commutator(X_train,Y_train,X_test,best_params,kernel='rbf',dist1='na',di
         else:
             if type(dist2)==str:
                 dist2=p_distance_commutator(X_train,X_test)
+            k=covariance(dist2,kernel,params)
+            return np.dot(k.T,alpha)
+
+def KRR_commutator_with_eig(X_train,Y_train,X_test,best_params,kernel='rbf',dist1='na',dist2='na'):
+    lam = best_params['lambda']
+    params = best_params
+    if type(dist1)==str:
+        dist1=p_distance_commutator(X_train,X_train) + p_distance_eigval_batch(X_train, X_train)
+    K=covariance(dist1,kernel,params)
+    K+=(np.eye(K.shape[0])*lam)
+    try:
+        L=np.linalg.cholesky(K)
+    except:
+        return 'Gram Matrix is not positive definite'
+    else:
+        try:
+            alpha=cho_solve((L,True),Y_train)
+        except:
+            return 'Cholesky decomposition failed, check distance matrices'
+        else:
+            if type(dist2)==str:
+                dist2=p_distance_commutator(X_train,X_test) + p_distance_eigval_batch(X_train, X_test)
             k=covariance(dist2,kernel,params)
             return np.dot(k.T,alpha)
 
